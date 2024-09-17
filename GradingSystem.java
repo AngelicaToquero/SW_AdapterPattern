b0153c3906df3c042a0a02928e3ec86b1b528fb3
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2024-09-17 09:50:24 +0800

    Update GradingSystem.java

--- GradingSystem.java
+++ GradingSystem.java
@@ -1,5 +1,5 @@
 public class GradingSystem {
     public void recordGrades() {
-        System.out.println("Student Number: 22-10329-294 , Input Grade in Software Engineering:__ ");
+        System.out.println("Student Number: 22-10329-294 \n\t\tInput Grade in Software Engineering:__ ");
     }
 }
